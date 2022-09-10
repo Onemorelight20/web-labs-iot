330603f9863c143f8319babcb12ab162c963835b
Bohdan Boretskyi <bogdanbor.etc@gmail.com>
2022-09-10 20:06:23 +0300

    init react project

--- lab-5-backend-calls/films-frontend/js/film_model.js
+++
@@ -1,8 +0,0 @@
-export class Film {
-  constructor(id, filmTitle, lengthOfFilm, reviewsAmount) {
-    this.id = id
-    this.filmTitle = filmTitle;
-    this.lengthOfFilm = lengthOfFilm;
-    this.reviewsAmount = reviewsAmount;
-  }
-}
